db69b8dc00d52f769444ff31eb3fe95bc20228d4
Alaaeldeen Hamed <mregydev@gmail.com>
2025-05-31 14:23:54 +0200

    chore: solve cls

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import List from './componenets/List';
-import PhotoDetails from './componenets/PhotoDetails';
+import Spinner from './componenets/Spinner';
 
+// Lazy load the PhotoDetails component
+const PhotoDetails = lazy(() => import('./componenets/PhotoDetails'));
 
 const App: React.FC = () => (
+  <Suspense fallback={<Spinner />}>
     <Routes>
-      <Route path="/" element={<List />} />
-      <Route path="/photo/:id" element={<PhotoDetails />} />
+      <Route path='/' element={<List />} />
+      <Route path='/photo/:id' element={<PhotoDetails />} />
     </Routes>
+  </Suspense>
 );
 
 export default App;
